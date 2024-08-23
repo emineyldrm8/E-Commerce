0d602ac7bb8735b8afa8b77aaa6ccd31c66693c6
DESKTOP-K3F0586\PC <emineyildiriim.2@gmail.com>
2024-08-23 14:29:01 +0300

    gereksiz repositoryler silindi,mapler düzenlendi

--- src/main/java/com/haratres/ecommerce/mapper/CartMapper.java
+++ src/main/java/com/haratres/ecommerce/mapper/CartMapper.java
@@ -2,10 +2,8 @@ package com.haratres.ecommerce.mapper;
 
 import com.haratres.ecommerce.dto.CartDto;
 import com.haratres.ecommerce.dto.CartEntryDto;
-import com.haratres.ecommerce.dto.ProductDto;
 import com.haratres.ecommerce.model.Cart;
 import com.haratres.ecommerce.model.CartEntry;
-import com.haratres.ecommerce.model.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -14,10 +12,12 @@ import java.util.List;
 
 @Mapper
 public interface CartMapper {
-    CartMapper INSTANCE= Mappers.getMapper(CartMapper.class);
+    CartMapper INSTANCE = Mappers.getMapper(CartMapper.class);
+
     CartDto toCartDto(Cart cart);
     Cart toCart(CartDto cartDto);
     List<CartEntryDto> toCartEntryDtoList(List<CartEntry> cartEntries);
+
     @Mapping(source = "product.id", target = "productId")
     @Mapping(source = "product", target = "product")
     CartEntryDto toCartEntryDto(CartEntry cartEntry);
